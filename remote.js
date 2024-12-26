af5ebdf7371173ace06ee57c7f4ffdc226ef53ab
abhijith2021 <Abhijithkishoretvm20@gmail.com>
2024-12-26 09:17:12 +0530

    2nd commit

--- remote.js
+++ remote.js
@@ -1,7 +1,7 @@
 let count = 0;
 function clicked(){
     count+=1;
-    console.log(count);
+    // console.log(count);
 if(count % 2 ==0 ){
     document.getElementById("input1").value = "off";
 
@@ -15,13 +15,35 @@ else{
 let volumeup = 0;
 function volume(){
     volumeup += 1;
-    console.log(volumeup);
+    // console.log(volumeup);
     document.getElementById("input1").value = "volume is: " + volumeup;
 }
 function volumedown(){
     volumeup -= 1;
     console.log(volumeup);
     document.getElementById("input1").value = "volume is: " + volumeup;
+}
+
+function clear(){
+    document.getElementById("input1").value = '';
+}
+
+function zero(){
+    document.getElementById("input1").value = 0;
+}
+function one(){
+    document.getElementById("input1").value = 1;
+}function two(){
+    document.getElementById("input1").value = 2;
+}
+function three(){
+    document.getElementById("input1").value = 3;
+}
+function four(){
+    document.getElementById("input1").value = 4;
+}
+function five(){
+    document.getElementById("input1").value = 5;
 }
 
 
